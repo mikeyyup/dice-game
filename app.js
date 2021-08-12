253f1c9ef712f4f0108b1e73de4b574506e639ea
Michael Perez <michaelperez@michaels-mbp-2.myfiosgateway.com>
2021-08-11 23:36:54 -0400

    final js blur effect/play again button

--- app.js
+++ app.js
@@ -5,13 +5,36 @@ const reset = document.getElementById('reset');
 let message = document.getElementById('win-banner');
 const score1 = document.getElementById('score1');
 const score2 = document.getElementById('score2');
+const title = document.getElementById('title');
 const dice = document.querySelectorAll('.dice');
 const modal = document.getElementsByTagName('div')[0];
 const playAgain = document.getElementById('play-again-btn');
-const winScreen = document.getElementsByClassName('players-cards');
+const playCards = document.getElementsByClassName('player-cards');
+const winScreen = document.querySelectorAll('.players-cards');
 
 let playing = true;
 let currentScore1, currentScore2, gamePlay;
+
+const winBlur = function () {
+  winScreen[0].classList.add('blur');
+  winScreen[1].classList.add('blur');
+  title.classList.add('blur');
+  title.textContent = 'Thanks for Playin :)';
+};
+
+const winner1 = function () {
+  message.textContent = 'PLAYER 1 WINS!';
+  document.getElementById('roll').disabled = true;
+  modal.classList.remove('hidden');
+  winBlur();
+};
+
+const winner2 = function () {
+  message.textContent = 'PLAYER 2 WINS!';
+  document.getElementById('roll').disabled = true;
+  modal.classList.remove('hidden');
+  winBlur();
+};
 
 const init = function () {
   score1.textContent = 0;
@@ -22,6 +45,11 @@ const init = function () {
   playing = true;
   message.innerHTML = 'The Dice Game';
   roll.classList.remove('hidden');
+  modal.classList.add('hidden');
+  winScreen[0].classList.remove('blur');
+  winScreen[1].classList.remove('blur');
+  title.classList.remove('blur');
+  title.textContent = 'The Dice Game';
 };
 
 init();
@@ -40,35 +68,21 @@ gamePlay = roll.addEventListener('click', function () {
   }
   // GAME LOGIC
   if (score1.innerHTML >= 10 && score2.innerHTML < 10) {
-    message.textContent = 'PLAYER 1 WINS!';
-    document.getElementById('roll').disabled = true;
-    modal.classList.remove('hidden');
-    winScreen.classList.add('blur');
+    winner1();
   } else if (score2.innerHTML >= 10 && score1.innerHTML < 10) {
-    message.textContent = 'PLAYER 2 WINS!';
-    document.getElementById('roll').disabled = true;
-    modal.classList.remove('hidden');
-    winScreen.classList.add('blur');
+    winner2();
   } else if (score1.innerHTML >= 10 && score2.innerHTML >= 10) {
     if (score1.innerHTML > score2.innerHTML) {
-      message.textContent = 'PLAYER 1 WINS!';
-      document.getElementById('roll').disabled = true;
-      modal.classList.remove('hidden');
-      winScreen.classList.add('blur');
+      winner1();
     } else if (score2.innerHTML > score1.innerHTML) {
-      message.textContent = 'PLAYER 2 WINS!';
-      document.getElementById('roll').disabled = true;
-      modal.classList.remove('hidden');
-      winScreen.classList.add('blur');
+      winner2();
     }
   }
 });
 
 // RESET BUTTON
 reset.addEventListener('click', init);
+// PLAY AGAIN BUTTON
+playAgain.addEventListener('click', init);
 
-playAgain.addEventListener('click', function () {
-  modal.classList.add('hidden');
-
-  init();
-});
+// console.log(winScreen);
